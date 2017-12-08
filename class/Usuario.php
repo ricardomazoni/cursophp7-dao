70c6fa8ee9a063667498e2d71aeeff3dd255b957
Ricardo Mazoni <ricardomazoni@gmail.com>
2017-12-08 15:32:01 -0200

    metodos de listas do usuarios

--- class/Usuario.php
+++ class/Usuario.php
@@ -55,6 +55,50 @@ class Usuario {
 
 
 	}
+	// Sendo statico não preciso ser iniciado...
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"	
+
+		));
+
+	}
+
+	public function login($login,$password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN and dessenha = :PASSWORD",array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if(count($results)>0){
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}else{
+			throw new Exception("Login e/ou senha invalidos");
+			
+		}
+
+
+	}
+
 
 	public function __toString(){
 		return json_encode(array(
--- index.php
+++ index.php
@@ -8,13 +8,35 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 
-*/
 
 $root = new Usuario();
-
 $root->loadbyId(1);
-
 echo $root;
+
+$lista = Usuario::getList();
+echo json_encode($lista);
+
+
+*/
+
+//Carrega uma lista de usuarios buscando os login
+
+/*$search = Usuario::search("jo");
+
+echo json_encode($search);
+
+*/
+
+//carrega o usuario usando usu e a senha
+
+$usuario = new Usuario();
+
+$usuario->login("jojo","12345");
+
+echo $usuario;
+
+
+
 
 
  ?>
